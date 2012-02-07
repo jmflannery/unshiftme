de35adab47f186b55305e06bf57a4116a4827840
Jack Flannery <jmflannery81@gmail.com>
2012-02-07 15:10:27 -0800

    fix attachment controller specs

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -7,17 +7,9 @@ class AttachmentsController < ApplicationController
       if @attachment.save
         format.js do
           @attachment.set_recievers
-          p @attachment.payload_file_name
           @message = current_user.messages.build(content: @attachment.payload_file_name, attachment_id: @attachment.id)
-          if @message.save
-            p "saved"
-            @message.set_recievers
-          else
-           p "un-saved" 
-          end
+          @message.set_recievers if @message.save
         end
-      else
-        render(action: :get)
       end
     end
   end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -3,48 +3,48 @@ require 'spec_helper'
 describe AttachmentsController do
    
   before(:each) do
-    @file = File.new(Rails.root + "spec/fixtures/files/test_file.txt")
+    @base_name = "test_file.txt"
+    @upload_file = fixture_file_upload("/files/" + @base_name, "text/plain")
   end
 
   describe "access control" do
 
     it "should deny access to 'create' for non-signed in users" do
-      post :create, attachment: {payload: @file} 
+      post :create, :attachment => {:payload => @upload_file}, format: :js 
       response.should redirect_to(signin_path)
     end
   end
   
-  describe "POST 'create'" do
+  describe "XHR POST 'create'" do
 
     before(:each) do
-      @user = test_sign_in(Factory(:user))
+      @user = test_sign_in(Factory(:user)) 
     end
     
     it "should create an attachment" do
       lambda do
-        post :create, attachment: {payload: @file}, format: :js
+        xhr :post, :create, :attachment => {:payload => @upload_file}, format: :jss
       end.should change(Attachment, :count).by(1)
     end
 
     it "should create an attachment that belongs to the current user" do
-      post :create, attachment: {payload: @file}, format: :js
+      xhr :post, :create, :attachment => {:payload => @upload_file}, format: :js
       assigns(:attachment).user_id.should == @user.id
     end
 
     it "should create a message" do
       lambda do
-        post :create, attachment: {payload: @file}, remote: true, format: :js
+        xhr :post, :create, :attachment => {:payload => @upload_file}, format: :js
       end.should change(Message, :count).by(1)
     end  
 
     it "should create a message with the file name as content" do
-      puts File.basename(@file)
-      post :create, :attachment => {:payload => @file}, format: :js 
-      assigns(:message).content.should == File.basename(@file)
-    end 
-
+      xhr :post, :create, :attachment => {:payload => @upload_file}, format: :js 
+      assigns(:message).content.should == @base_name
+    end
+ 
     it "should link the newly created message and attachment" do
-      post :create, attachment: {payload: @file}, remote: true, format: :js 
+      xhr :post, :create, :attachment => {:payload => @upload_file}, format: :js
       assigns(:message).attachment_id.should == assigns(:attachment).id
     end
   end
