70d04248d024ff8b63751bdb0406019774b3101a
Jack Flannery <jmflannery81@gmail.com>
2012-02-15 21:39:48 -0800

    add unicorn gem and unicorn config and other config changes

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/jack/rails/chatty_pants"
+pid "/home/jack/rails/chatty_pants/tmp/pids/unicorn.pid"
+stderr_path "/home/jack/rails/chatty_pants/log/unicorn.log"
+stdout_path "/home/jack/rails/chatty_pants/log/unicorn.log"
+
+listen "/tmp/unicorn.chatty_pants.sock"
+worker_processes 2
+timeout 30
