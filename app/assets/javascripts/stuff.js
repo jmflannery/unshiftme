729845982b2b1fb64ac1fe302bc60aee254dac77
Jack Flannery <jmflannery81@gmail.com>
2012-04-12 00:08:01 -0400

    redesign part 2, flexible height

--- app/assets/javascripts/stuff.js
+++ app/assets/javascripts/stuff.js
@@ -1,18 +1,50 @@
+////////////////////////////////////////
+// Calculate Height
+////////////////////////////////////////
+
+$(function() {
+  calculate_message_section_height();
+});
+
+$(function() {
+  $(window).resize(calculate_message_section_height);
+});
+
+var calculate_message_section_height = function() {
+  height = $(window).outerHeight(true);
+  console.log("height=" + height);
+  calculated_height = (height * 68) / 100;  
+  $('#messages_section').height(calculated_height);
+  $('#messages_section').scrollTo("max");
+}; 
+
 ///////////////////////////////////
 // Recipient User Selection
 ///////////////////////////////////
 
 var toggleRecipientSelectionSection = function() {
+  // get recipient selection section element
   recipient_selection_section = $('#recipient_selection_section');
+  // toggle visibility
   recipient_selection_section.toggle();
+  // toggle visibility class
   recipient_selection_section.toggleClass("visible");
-
+  // get the height
   height = recipient_selection_section.outerHeight(true);
+
+  // get users button element
+  users_button = $('a#users_button');
     
   if (recipient_selection_section.is(":hidden")) {
+    // increase messages section height
     $('#messages_section').css("height", "+=" + height);
+    // change users button text
+    users_button.text("Show Available Users");
   } else {
+    // reduce messages section height
     $('#messages_section').css("height", "-=" + height);   
+    // change users button text
+    users_button.text("Hide Available Users");
   }
 
   $('#messages_section').scrollTo("max");
@@ -42,12 +74,15 @@ $(function() {
     height = upload_section.outerHeight(true);
     messages_section = $('#messages_section');
     upload_section.toggle();
+    upload_button = $('a#attach_button');
 
     if (upload_section.is(":hidden")) {
       messages_section.css("height", "+=" + height);
       $('input[type="file"]').val("");
+      upload_button.text("Show File Uploader");
     } else {
       messages_section.css("height", "-=" + height);
+      upload_button.text("Hide File Uploader");
     }
   });
 });
