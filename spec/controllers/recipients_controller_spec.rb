e904346760bd942e009581ccf4ab803fcbcd29e8
Jack Flannery <jmflannery81@gmail.com>
2012-05-29 21:51:05 -0400

    refactor recipients controller spec

--- spec/controllers/recipients_controller_spec.rb
+++ spec/controllers/recipients_controller_spec.rb
@@ -2,43 +2,36 @@ require 'spec_helper'
 
 describe RecipientsController do  
   render_views
-
-  describe "access control" do
-
-    it "should deny access to 'create' for non-signed in users" do
-      post :create, :format => :js
-      response.should redirect_to(signin_path)
-    end
-
-    it "should deny access to 'destroy' for non-signed in users" do
-      delete :destroy, :id => 0, :format => :js
-      response.should redirect_to(signin_path)
-    end
-  end 
-
+  
   describe "POST 'create'" do
 
     let(:user) { FactoryGirl.create(:user) }
     let(:cusn) { FactoryGirl.create(:desk, name: "CUS North", abrev: "CUSN", job_type: "td") }
-    before(:each) { test_sign_in(user) }
 
-    it "should be success" do
-      post :create, desk_id: cusn.id, format: :js
-      response.should be_success
+    context "for non-signed in users" do
+      it "denies access" do
+        post :create, desk_id: cusn.id, format: :js
+        response.should redirect_to(signin_path)
+      end
     end
 
-    describe "failure" do
-
-      it "should create not a recipient without a valid desk id" do
+    context "without a valid desk id" do
+      before(:each) { test_sign_in(user) }
+      it "creates not a recipient" do
         lambda do
           post :create, desk_id: 0, format: :js
         end.should_not change(Recipient, :count)
       end
     end
 
-    describe "success" do
+    context "with valid desk_id" do
+      before(:each) { test_sign_in(user) }
+      it "returns http success" do
+        post :create, desk_id: cusn.id, format: :js
+        response.should be_success
+      end
 
-      it "should create a recipient given a desk id" do
+      it "creates a recipient" do
         lambda do
           post :create, desk_id: cusn.id, format: :js
         end.should change(Recipient, :count).by(1)
@@ -48,29 +41,41 @@ describe RecipientsController do
 
   describe "DELETE 'destroy'" do
    
-    before(:each) do
-      @user = test_sign_in(FactoryGirl.create(:user))
-      @recipient = FactoryGirl.create(:recipient, user: @user)
+
+    context "for a non-signed in user" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @recipient = FactoryGirl.create(:recipient, user: @user)
+      end
+      it "denies access" do
+        delete :destroy, id: @recipient.id, format: :js
+        response.should redirect_to(signin_path)  
+      end
     end
 
-    it "should be successful" do
-      delete :destroy, id: @recipient.id, format: :js
-      response.should be_success
-    end
-
-    describe "for an unauthorized user" do
-
-      before(:each) { test_sign_in(FactoryGirl.create(:user)) }
-
-      it "should deny access" do
+    context "for an unauthorized user" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @recipient = FactoryGirl.create(:recipient, user: @user)
+        test_sign_in(FactoryGirl.create(:user))
+      end
+      it "denies access" do
         delete :destroy, id: @recipient.id, format: :js
         response.should redirect_to(root_path)  
       end
     end
 
-    describe "for an authorized user" do
+    context "for an authorized user" do
+      before(:each) do
+        @user = test_sign_in(FactoryGirl.create(:user))
+        @recipient = FactoryGirl.create(:recipient, user: @user)
+      end
+      it "returns http success" do
+        delete :destroy, id: @recipient.id, format: :js
+        response.should be_success
+      end
 
-      it "should destroy a recipient" do
+      it "destroys a recipient" do
         lambda do
           delete :destroy, id: @recipient.id, format: :js
         end.should change(Recipient, :count).by(-1)
