ec4fd27a23b9b68fd52c82b215428623f6e8518d
Jack Flannery <jmflannery81@gmail.com>
2012-07-04 16:33:11 -0400

    fix attachments factory

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :attachment, class: Attachment do |attachment|
-    attachment.payload Rails.root + "spec/fixtures/files/test_file.txt"
+    attachment.payload File.new(Rails.root + "spec/fixtures/files/test_file.txt")
     association :user, factory: :user
   end
 end
