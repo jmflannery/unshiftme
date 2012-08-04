7797df436d593ae99d87244a07939f3ce93a5424
Jack Flannery <jmflannery81@gmail.com>
2012-08-04 09:45:57 -0400

    remove old route map comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 AmtrakMessenger::Application.routes.draw do
   root :to => "sessions#new"
 
-  resources :users, :only => [:new, :create, :show, :index, :edit, :update] 
+  resources :users, :only => [:new, :create, :show, :index, :edit, :update]
   resources :messages, :only => [:create, :index, :update]
   resources :recipients, :only => [:create, :index, :destroy]
   resources :attachments, :only => [:create]
@@ -14,24 +14,3 @@ AmtrakMessenger::Application.routes.draw do
   match "/signout", :to => "sessions#destroy"
 end
 
-#== Route Map
-# Generated on 06 Feb 2012 20:19
-#
-#       users GET    /users(.:format)          users#index
-#             POST   /users(.:format)          users#create
-#    new_user GET    /users/new(.:format)      users#new
-#   edit_user GET    /users/:id/edit(.:format) users#edit
-#        user GET    /users/:id(.:format)      users#show
-#             PUT    /users/:id(.:format)      users#update
-#    messages GET    /messages(.:format)       messages#index
-#             POST   /messages(.:format)       messages#create
-#  recipients GET    /recipients(.:format)     recipients#index
-#             POST   /recipients(.:format)     recipients#create
-#   recipient DELETE /recipients/:id(.:format) recipients#destroy
-# attachments POST   /attachments(.:format)    attachments#create
-#     session POST   /session(.:format)        sessions#create
-# new_session GET    /session/new(.:format)    sessions#new
-#             DELETE /session(.:format)        sessions#destroy
-#      signup        /signup(.:format)         users#new
-#      signin        /signin(.:format)         sessions#new
-#     signout        /signout(.:format)        sessions#destroy
