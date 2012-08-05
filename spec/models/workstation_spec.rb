07faa12997a5c0d5c7856db1d5d193a9ad88041b
Jack Flannery <jmflannery81@gmail.com>
2012-08-04 20:29:07 -0400

    refactor workstation spec

--- spec/models/workstation_spec.rb
+++ spec/models/workstation_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Workstation do
+  
+  let(:user) { FactoryGirl.create(:user, user_name: "epresley") }
 
-  before(:each) do
-    @cusn = Workstation.create(name: "CUS North", abrev: "CUSN", job_type: "td")
-    @cuss = Workstation.create(name: "CUS South", abrev: "CUSS", job_type: "td")
-    @aml = Workstation.create(name: "AML / NOL", abrev: "AML", job_type: "td")
-    @ydctl = Workstation.create(name: "Yard Control", abrev: "YDCTL", job_type: "ops")
-    @ydmstr = Workstation.create(name: "Yard Master", abrev: "YDMSTR", job_type: "ops")
-    @glhs = Workstation.create(name: "Glasshouse", abrev: "GLHSE", job_type: "ops")
-  end
+  let!(:cusn) { Workstation.create(name: "CUS North", abrev: "CUSN", job_type: "td") }
+  let!(:cuss) { Workstation.create(name: "CUS South", abrev: "CUSS", job_type: "td") }
+  let!(:aml) { Workstation.create(name: "AML / NOL", abrev: "AML", job_type: "td") }
+  let!(:ydctl) { Workstation.create(name: "Yard Control", abrev: "YDCTL", job_type: "ops") }
+  let!(:ydmstr) { Workstation.create(name: "Yard Master", abrev: "YDMSTR", job_type: "ops") }
+  let!(:glhs) { Workstation.create(name: "Glasshouse", abrev: "GLHSE", job_type: "ops") }
 
-  subject { @cusn }
+  subject { cusn }
 
   it { should respond_to(:name) }
   it { should respond_to(:abrev) }
@@ -29,18 +29,18 @@ describe Workstation do
       end
 
       it "returns a list of all Workstations of the given type" do
-        @td_workstations.should include @cusn
-        @ops_workstations.should_not include @cusn
-        @td_workstations.should include @cuss
-        @ops_workstations.should_not include @cuss
-        @td_workstations.should include @aml
-        @ops_workstations.should_not include @aml
-        @ops_workstations.should include @ydctl
-        @td_workstations.should_not include @ydctl
-        @ops_workstations.should include @ydmstr
-        @td_workstations.should_not include @ydmstr
-        @ops_workstations.should include @glhs
-        @td_workstations.should_not include @glhs
+        @td_workstations.should include cusn
+        @ops_workstations.should_not include cusn
+        @td_workstations.should include cuss
+        @ops_workstations.should_not include cuss
+        @td_workstations.should include aml
+        @ops_workstations.should_not include aml
+        @ops_workstations.should include ydctl
+        @td_workstations.should_not include ydctl
+        @ops_workstations.should include ydmstr
+        @td_workstations.should_not include ydmstr
+        @ops_workstations.should include glhs
+        @td_workstations.should_not include glhs
       end
     end
 
@@ -62,25 +62,21 @@ describe Workstation do
 
     describe "#description" do
 
-      let(:user) { FactoryGirl.create(:user, user_name: "epresley") }
 
       before(:each) do
-        @cusn = Workstation.create!(name: "CUS North", abrev: "CUSN", job_type: "td")
-        @cuss = Workstation.create!(name: "CUS South", abrev: "CUSS", job_type: "td")
-        user.authenticate_workstation(@cusn.abrev => 1)
+        cusn = Workstation.create!(name: "CUS North", abrev: "CUSN", job_type: "td")
+        cuss = Workstation.create!(name: "CUS South", abrev: "CUSS", job_type: "td")
+        user.authenticate_workstation(cusn.abrev => 1)
       end
     
       it "returns a string of the workstation name and workstation user (if the workstation has a user)" do
-        @cusn.description == "#{@cusn.name} (#{user.user_name})"
-        @cuss.description == "#{@cuss.name}"
+        cusn.description == "#{cusn.name} (#{user.user_name})"
+        cuss.description == "#{cuss.name}"
       end 
     end
   end
 
   describe "#view_class" do
-
-    let(:user) { FactoryGirl.create(:user, user_name: "epresley") }
-    let(:cusn) { FactoryGirl.create(:workstation, name: "CUS North", abrev: "CUSN", job_type: "td") }
 
     context "when the workstation is owned by the given user" do
       before { user.start_job(cusn.abrev) }
@@ -90,9 +86,9 @@ describe Workstation do
     end
 
     context "when the workstation is a recipient of the given user" do
-      before { @recip = FactoryGirl.create(:recipient, user: user, workstation_id: cusn.id) }
+      let!(:recip) { @recip = FactoryGirl.create(:recipient, user: user, workstation_id: cusn.id) }
       it "should have the right view class" do
-        cusn.view_class(user).should == "recipient_workstation on #{@recip.id}"
+        cusn.view_class(user).should == "recipient_workstation on #{recip.id}"
       end
     end
     
@@ -105,14 +101,18 @@ describe Workstation do
 
   describe "class method" do
 
+    describe "as_json" do
+
+    end
+
     describe "all_short_names" do
       it "should return a list of all the workstation abrevs of all workstations in the system" do
-        Workstation.all_short_names.should include @cusn.abrev
-        Workstation.all_short_names.should include @cuss.abrev
-        Workstation.all_short_names.should include @aml.abrev
-        Workstation.all_short_names.should include @ydctl.abrev
-        Workstation.all_short_names.should include @ydmstr.abrev
-        Workstation.all_short_names.should include @glhs.abrev
+        Workstation.all_short_names.should include cusn.abrev
+        Workstation.all_short_names.should include cuss.abrev
+        Workstation.all_short_names.should include aml.abrev
+        Workstation.all_short_names.should include ydctl.abrev
+        Workstation.all_short_names.should include ydmstr.abrev
+        Workstation.all_short_names.should include glhs.abrev
       end
     end
   end
