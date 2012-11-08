675675d4ad5c2ef27fed24f14045238a7ce4acb5
Jack Flannery <jmflannery81@gmail.com>
2012-11-08 07:54:20 -0500

    add merge_workstation_params to workstatation helper

--- app/helpers/workstations_helper.rb
+++ app/helpers/workstations_helper.rb
@@ -13,4 +13,11 @@ module WorkstationsHelper
     end
     workstations
   end
+
+  def merge_workstation_params(params_hash)
+    workstations = parse_params_for_workstations(params_hash)
+    params_hash[:user][:normal_workstations] = workstations if params_hash.has_key?(:user)
+    params_hash
+  end
 end
+
--- spec/helpers/workstations_helper_spec.rb
+++ spec/helpers/workstations_helper_spec.rb
@@ -3,16 +3,26 @@ require 'spec_helper'
 describe "WorkstationssHelper" do
   include WorkstationsHelper
 
+  let(:params) { { user: { user_name: "Mit" }, a_key: "a value", "CUSN" => "1", "AML" => "1", another_key: "another value" } }
+
+  before(:each) do
+    FactoryGirl.create(:workstation, name: "CUS North", abrev: "CUSN")
+    FactoryGirl.create(:workstation, name: "AML / NOL", abrev: "AML")
+  end
+
   describe "#parse_params_for_workstations" do
 
-    let(:params) { { a_key: "a_value", "CUSN" => "1", "AML" => "1", another_key: "another value" } }
-    before(:each) do
-      FactoryGirl.create(:workstation, name: "CUS North", abrev: "CUSN")
-      FactoryGirl.create(:workstation, name: "AML / NOL", abrev: "AML")
+    it "parses the params hash and returns an array of the found workstation abrevs" do
+      parse_params_for_workstations(params).should == %w(CUSN AML)
     end
+  end
 
-    it "parses the params hash and returns and array the found workstations" do
-      parse_params_for_workstations(params).should == ["CUSN", "AML"]
+  describe "#merge_workstation_params" do
+    
+    it "merges the normal workstations array into the params[:user] hash" do
+      merged_params = merge_workstation_params(params)
+      merged_params[:user].should have_key :normal_workstations
+      merged_params[:user][:normal_workstations].should == %w(CUSN AML)
     end
   end
 end
