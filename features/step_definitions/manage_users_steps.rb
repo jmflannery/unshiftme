daba0765b86604aa56ded35ecc4d049f40ddf1fa
Jack Flannery <jmflannery81@gmail.com>
2012-11-12 20:52:19 -0500

    add cucumber steps to see that admin users have the checkbox checked on the manage users page

--- features/step_definitions/manage_users_steps.rb
+++ features/step_definitions/manage_users_steps.rb
@@ -1,16 +1,22 @@
 Then /^I should see user records for "(.*?)"$/ do |user_names|
-  users = user_names.split(",")
-  users.each do |user_name|
+  user_names.split(",").each do |user_name|
     within("li.#{user_name}") do
-      page.should have_selector("p", text: user_name)
+      page.should have_content(user_name)
     end
   end
 end
 
-Then /^I should see that user "(.*?)" is an admin user$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see that user "(.*?)" is an admin user$/ do |user_name|
+  within("li.#{user_name}") do
+    find("input##{user_name}").should be_checked
+  end
 end
 
-Then /^I should see that users "(.*?)" are not admin users$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see that users "(.*?)" are not admin users$/ do |user_names|
+  user_names.split(",").each do |user_name|
+    within("li.#{user_name}") do
+      find("input##{user_name}").should_not be_checked
+    end
+  end
 end
+
