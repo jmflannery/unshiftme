2d34b0e2c0f74040c1dc033e160130ea8830cca8
Jack Flannery <jmflannery81@gmail.com>
2012-11-17 13:16:10 -0500

    add updating_user_admin_status? method to users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
   
+  def updating_user_admin_status?
+    params.has_key?("user") and params["user"].has_key?("admin")
+  end
+
   def deletion_confirmed?
     params.has_key?("commit") and params["commit"] =~ /Yes delete user/
   end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -3,6 +3,27 @@ require 'spec_helper'
 describe "UsersHelper" do
   include UsersHelper
 
+  describe "#updating_user_admin_status?" do
+
+    context "if params hash contains user[:admin]" do
+
+      let(:params) {{ "user" => { "admin" => "1" } }}
+
+      it "returns true"  do
+        updating_user_admin_status?.should be_true
+      end
+    end
+
+    context "if params hash does not contain user[:admin]" do
+
+      let(:params) {{ "user" => { "user_name" => "bill", "password" => "secret", "password_confirmation" => "secret" } }}
+
+      it "returns false"  do
+        updating_user_admin_status?.should be_false
+      end
+    end
+  end
+
   describe "#deletion_confirmed?" do
 
     let(:params) { { user: { user_name: "frank" }, a_key: "a value" } }
