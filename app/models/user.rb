88013fc10d5879025a780495a91bae5d64aaecf0
Jack Flannery <jmflannery81@gmail.com>
2012-11-18 20:54:57 -0500

    add admin to attr_accessible in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   has_secure_password 
 
-  attr_accessible :user_name, :password, :password_confirmation, :normal_workstations
+  attr_accessible :user_name, :password, :password_confirmation, :normal_workstations, :admin
  
   has_many :workstations
   has_many :messages
