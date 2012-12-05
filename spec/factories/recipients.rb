8686ce1476c39c227d64a112745b69e93279e5e5
Jack Flannery <jmflannery81@gmail.com>
2012-12-04 20:59:07 -0500

    fix recipient factory

--- spec/factories/recipients.rb
+++ spec/factories/recipients.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :recipient, class: Recipient do |recipient|
-    recipient.workstation_id 22
     association :user, factory: :user
+    association :workstation, factory: :workstation
   end
 end
+
