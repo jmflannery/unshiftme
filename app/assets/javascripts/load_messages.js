2ccf41fa9206ecf7ca7ae84afeef3266140a0fe7
Jack Flannery <jmflannery81@gmail.com>
2012-12-30 13:19:10 -0500

    fix the code style of load messages js

--- app/assets/javascripts/load_messages.js
+++ app/assets/javascripts/load_messages.js
@@ -2,21 +2,31 @@
 // Load messages
 ///////////////////////////////////////////////
 
-var load_messages = function() {
+function load_messages() {
   show_message_loading_icon();
+
   $.get("/messages.json", function(data) {
     hide_message_loading_icon();
+
     $.each(data, function(index, value) {
       if (value.view_class.search("owner") > 0) {
         readers = value.readers;
       } else {
         readers = "";
       }
-      var html = build_message(value.sender, value.attachment_url, value.content, value.created_at, value.view_class, readers);
+      var html = build_message(
+        value.sender,
+        value.attachment_url,
+        value.content,
+        value.created_at,
+        value.view_class,
+        readers
+      );
+
       display_message(html, value.id);
     });
   });
-};
+}
 
 $(function() {
   if (on_messaging_page()) {
