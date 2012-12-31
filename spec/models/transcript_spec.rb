b5e2c7493b8e52edb10e9f0a2171bfe260960c66
Jack Flannery <jmflannery81@gmail.com>
2012-12-31 13:36:38 -0500

    minor refactor of Transcript specs

--- spec/models/transcript_spec.rb
+++ spec/models/transcript_spec.rb
@@ -10,12 +10,15 @@ describe Transcript do
 
   subject { @transcript }
 
+  it { should respond_to(:user_id) }
   it { should respond_to(:transcript_user_id) }
   it { should respond_to(:transcript_workstation_id) }
   it { should respond_to(:start_time) }
   it { should respond_to(:end_time) }
 
   it { should be_valid }
+
+  it { should belong_to(:user) }
 
   describe "when start_time is not present" do
     before { @transcript.start_time = nil }
@@ -58,16 +61,6 @@ describe Transcript do
     end
   end
 
-  describe "user associations" do
-
-    it { should respond_to(:user) }
-
-    it "should have the right user associated user" do
-      @transcript.user_id.should == @user.id
-      @transcript.user.should == @user
-    end
-  end
-
   describe "named scope" do
 
     before(:each) do
@@ -90,7 +83,7 @@ describe Transcript do
 
   describe "#name" do
 
-    context "with a transcript user and workstation" do
+    context "when transcript_user and transcript_workstation are present" do
       let(:transcript_user) { FactoryGirl.create(:user, user_name: "jack") }
       let(:cusn) { FactoryGirl.create(:workstation, name: "CUS North", abrev: "CUSN", job_type: "td") }
       before do 
@@ -98,31 +91,31 @@ describe Transcript do
         subject.update_attribute(:transcript_workstation_id, cusn.id)
       end
 
-      it "returns the name including the transcript user and workstation and start and end_times" do
+      it "returns the full name of the transcript" do
         subject.name.should == "Transcript for CUSN jack from Jun 22 2012 16:30 to Jun 22 2012 17:15"
       end
     end
 
-    context "with a transcript user and no workstation" do
+    context "when only transcript_user is present" do
       let(:transcript_user) { FactoryGirl.create(:user, user_name: "jack") }
       before do 
         subject.update_attribute(:transcript_user_id, transcript_user.id)
         subject.update_attribute(:transcript_workstation_id, 0)
       end
 
-      it "returns the name including the transcript user and start and end_times" do
+      it "returns the full name of the transcript excluding transcript workstation" do
         subject.name.should == "Transcript for jack from Jun 22 2012 16:30 to Jun 22 2012 17:15"
       end
     end
 
-    context "with a trancript workstation and no user" do
+    context "when only transcript_workstation is present" do
       let(:cusn) { FactoryGirl.create(:workstation, name: "CUS North", abrev: "CUSN", job_type: "td") }
       before do 
         subject.update_attribute(:transcript_workstation_id, cusn.id)
         subject.update_attribute(:transcript_user_id, 0)
       end
 
-      it "returns the name including the transcript workstation and start and end_times" do
+      it "returns the full name of the transcript excluding transcript user" do
         subject.name.should == "Transcript for CUSN from Jun 22 2012 16:30 to Jun 22 2012 17:15"
       end
     end
@@ -138,7 +131,7 @@ describe Transcript do
         workstation: cusn.id
       }}
 
-      context "with a transcript user and workstation" do
+      context "when transcript_user and transcript_workstation are present" do
         before do 
           subject.update_attribute(:transcript_user_id, transcript_user.id)
           subject.update_attribute(:transcript_workstation_id, cusn.id)
@@ -149,7 +142,7 @@ describe Transcript do
         end
       end
 
-      context "with a transcript user and no workstation" do
+      context "when only transcript_user is present" do
         before do 
           subject.update_attribute(:transcript_user_id, transcript_user.id)
           expected.delete(:workstation)
@@ -160,7 +153,7 @@ describe Transcript do
         end
       end
 
-      context "with a transcript workstation and no user" do
+      context "when only transcript_workstation is present" do
         before do 
           subject.update_attribute(:transcript_workstation_id, cusn.id)
           expected.delete(:user)
