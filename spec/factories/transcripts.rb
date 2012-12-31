40de06864fde420e4e906a5fa4f007bfc2c61989
Jack Flannery <jmflannery81@gmail.com>
2012-12-31 13:58:09 -0500

    update the Transcript factory

--- spec/factories/transcripts.rb
+++ spec/factories/transcripts.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :transcript, class: Transcript do |transcript|
-    transcript.transcript_user_id 22
-    transcript.transcript_workstation_id 11
-    transcript.start_time "2012-04-29 17:52:39"
-    transcript.end_time "2012-04-29 18:52:39"
     association :user, factory: :user
+    association :transcript_user, factory: :user
+    association :transcript_workstation, factory: :workstation
+    transcript.start_time 5.hours.ago
+    transcript.end_time 1.minute.ago
   end
 end
