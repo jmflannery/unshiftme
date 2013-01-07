b9d3726bc69343d52f95d8e8d6e7d10a069f25a7
Jack Flannery <jmflannery81@gmail.com>
2013-01-06 19:55:07 -0500

    load the transcript messages from the transcript show url

--- app/assets/javascripts/load_transcripts.js
+++ app/assets/javascripts/load_transcripts.js
@@ -2,18 +2,18 @@
 // Load Transcript Messages
 ///////////////////////////////////////////////
 
-var load_transcript_messages = function() {
-//show_message_loading_icon();
+function load_transcript_messages() {
+  show_message_loading_icon();
   var ts_page = $('#transcript_show');
-  var data = {};
-  data["start_time"] = ts_page.data("startTime");
-  data["end_time"] = ts_page.data("endTime");
-  data["user_id"] = ts_page.data("user");
-  data["workstation_id"] = ts_page.data("workstation");
-  $.get("/messages.json", data, function(data) {
-  //  hide_message_loading_icon();
-    $.each(data, function(index, value) {
-      var html = build_message(value.sender, value.attachment_id, value.content, value.created_at, value.view_class, value.readers);
+  $.get("/transcripts/" + ts_page.data("id") + ".json", function(data) {
+    hide_message_loading_icon();
+    $.each(data.messages, function(index, value) {
+      var html = build_message(value.sender,
+                               value.attachment_id,
+                               value.content,
+                               value.created_at,
+                               value.view_class,
+                               value.readers);
       display_message(html, value.id);
     });
   });
