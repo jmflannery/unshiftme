55121017299d9fb4475894f7f23f1545e5be0e25
Jack Flannery <jmflannery81@gmail.com>
2013-03-17 12:13:48 -0400

    send the transcript: true option to the MessagePresenter to generate the message json for transcripts

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -54,7 +54,7 @@ class Transcript < ActiveRecord::Base
     json[:end_time] = end_time.to_s
     json[:user] = transcript_user_id if transcript_user_id
     json[:workstation] = transcript_workstation_id if transcript_workstation_id
-    json[:messages] = display_messages.map { |msg| MessagePresenter.new(msg, options[:user]).as_json }
+    json[:messages] = display_messages.map { |msg| MessagePresenter.new(msg, options[:user]).as_json(transcript: true) }
     json.as_json
   end
 end
