bb107dd8972a81edca92240cb590f25b8138dbbf
Jack Flannery <jmflannery81@gmail.com>
2013-05-22 17:48:03 -0400

    add attachments#index route and add link to it from main nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ AmtrakMessenger::Application.routes.draw do
   end
   resources :messages, :only => [:create, :index, :update]
   resources :message_routes, :only => [:create, :index, :destroy]
-  resources :attachments, :only => [:create]
+  resources :attachments, :only => [:create, :index]
   resources :transcripts, :only => [:new, :create, :show, :index, :destroy]
   resources :workstations, only: [:index]
   resource :session, :only => [:new, :create, :update, :destroy]
