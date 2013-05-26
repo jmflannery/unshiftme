6a7d17202653b9a61ad8f405a61bf621cb3bcdce
Jack Flannery <jmflannery81@gmail.com>
2013-05-26 12:39:47 -0400

    clean up attachments controller create action

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,12 +2,8 @@ class AttachmentsController < ApplicationController
   before_filter :authenticate   
    
   def create
-    @user = current_user
-    @message = current_user.create_attached_message(params[:attachment])#.attachments.build(params[:attachment]
-    #@attachment = @message.attachment
+    @message = current_user.create_attached_message(params[:attachment])
     if @message and @message.attachment
-      #@message = @user.messages.create(content: @attachment.payload_identifier)
-      #@message.attach(@attachment)
       @message.generate_outgoing_receipt
       @message.generate_incoming_receipts(attachment: @message.attachment)
       Pusher.push_message(@message)
