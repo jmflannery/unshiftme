04f9014524aa3503b51132937cd085e7d84a3c62
Jack Flannery <jmflannery81@gmail.com>
2013-06-02 19:40:11 -0400

    fix Attachment#for_user to return attachments sent to the users workstations

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -15,7 +15,8 @@ class Attachment < ActiveRecord::Base
   end
 
   def self.for_user(user)
-    irs = IncomingReceipt.where("user_id = ? and attachment_id is not null", user.id)
+    irs = IncomingReceipt.where("(user_id = ? or workstation_id in (?)) and attachment_id is not null",
+        user.id, user.workstation_ids)
     attachments = irs.map { |r| r.attachment }
     ogs = OutgoingReceipt.where("user_id = ?", user.id)
     ogs.select! { |r| r.message.attachment }
