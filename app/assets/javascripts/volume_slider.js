d93c2b23be9e6028d38495f5ea26df413ec564ad
Jack Flannery <jmflannery81@gmail.com>
2013-09-28 00:42:03 -0400

    enhance the volume slider

--- app/assets/javascripts/volume_slider.js
+++ app/assets/javascripts/volume_slider.js
@@ -1,15 +1,41 @@
 var VolumeSlider = function(slider, button) {
+  this.INITIAL_VOLUME = 50;
   this.$volume_slider = $(slider);
   this.$volume_button = $(button);
+  this.$tooltip = $("<div id='tooltip'/>");
+  this.$tooltip.hide();
   this.$slider = $("<div id='slider'/>");
-  this.$volume_slider.append(this.$slider);
+  this.$volume_slider.append(this.$tooltip, this.$slider);
   this.bindEvents();
   this.$volume_slider.hide();
+  this.setVolumeIcon(this.INITIAL_VOLUME);
+
+  this.slider_attrs = {
+    animate: "fast",
+    orientation: "vertical",
+    range: "min",
+    value: this.INITIAL_VOLUME,
+
+    start: function() {
+      this.$tooltip.fadeIn('fast');
+    }.bind(this),
+
+    slide: function(event, ui) {
+      var value = this.$slider.slider('value');
+      this.$tooltip.css('bottom', value).text(ui.value);
+      this.setVolumeIcon(ui.value);
+    }.bind(this),
+
+    stop: function() {
+      this.$tooltip.fadeOut('fast');
+    }.bind(this),
+  };
 };
 
 VolumeSlider.prototype = {
+
   setup: function() {
-    this.$slider.slider({ animate: "fast", orientation: "vertical" });
+    this.$slider.slider(this.slider_attrs);
   },
 
   bindEvents: function() {
@@ -28,6 +54,16 @@ VolumeSlider.prototype = {
     e.preventDefault();
     this.$volume_button.removeClass('active');
     this.$volume_slider.hide();
+  },
+
+  setVolumeIcon: function(val) {
+    if (val >= 50) {
+      this.$volume_button.html("<i class='icon-volume-up'></i>");
+    } else if (val >= 1 && val < 50) {
+      this.$volume_button.html("<i class='icon-volume-down'></i>");
+    } else if (val == 0) {
+      this.$volume_button.html("<i class='icon-volume-off'></i>");
+    }
   },
 
   setSliderPosition: function() {
@@ -49,4 +85,3 @@ VolumeSlider.prototype = {
 $(function() {
   new VolumeSlider('#volume_slider', '#volume_button').setup();
 });
-
