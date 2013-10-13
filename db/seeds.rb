16849c37d136f7b3ff816f5c0c900cead9d416f6
Jack Flannery <jmflannery81@gmail.com>
2013-10-13 10:46:14 -0400

    create an admin user in the db seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+User.create(user_name: 'bill', password: 'bbbbbb', password_confirmation: 'bbbbbb', admin: true)
+
 Workstation.delete_all
-Workstation.create!(name: "CUS North", abrev: "CUSN", job_type: "td")
-Workstation.create!(name: "CUS South", abrev: "CUSS", job_type: "td")
-Workstation.create!(name: "AML / NOL", abrev: "AML", job_type: "td")
-Workstation.create!(name: "Yard Control", abrev: "YDCTL", job_type: "ops")
-Workstation.create!(name: "Yard Master", abrev: "YDMSTR", job_type: "ops")
-Workstation.create!(name: "Glasshouse", abrev: "GLHSE", job_type: "ops")
-Workstation.create!(name: "CCC", abrev: "CCC", job_type: "ops")
+Workstation.create(name: "CUS North", abrev: "CUSN", job_type: "td")
+Workstation.create(name: "CUS South", abrev: "CUSS", job_type: "td")
+Workstation.create(name: "AML / NOL", abrev: "AML", job_type: "td")
+Workstation.create(name: "Yard Control", abrev: "YDCTL", job_type: "ops")
+Workstation.create(name: "Yard Master", abrev: "YDMSTR", job_type: "ops")
+Workstation.create(name: "Glasshouse", abrev: "GLHSE", job_type: "ops")
+Workstation.create(name: "CCC", abrev: "CCC", job_type: "ops")
  
